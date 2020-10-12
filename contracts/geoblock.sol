e2f0d9cdd9cba30ba13979bf1d32f50035a2f484
Logan Warner <logan@true.group>
2020-10-13 11:32:08 +1300

    Not sure where I was up to so just going to commit and work from there

--- contracts/geoblock.sol
+++ contracts/geoblock.sol
@@ -1,2 +1,28 @@
 pragma solidity >= 0.7.0 < 0.8.0;
 
+contract Geoblock {
+	struct User {
+		int32 level;
+		string name;
+	};
+
+	struct GeoCache {
+		string latitude;
+		string longitude;
+		string salt;e
+		bytes32 hash;
+	};
+
+	mapping(address => User) public users;
+	mapping(uint => address) public erc721ToOwner;
+	
+	function getUser(address userAddress) public view returns {
+		return users[userAddress];
+	}
+
+	function getErc721(address userAddress) {
+		
+	}
+
+	
+}
